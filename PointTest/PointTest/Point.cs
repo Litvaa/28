b457c5bb54892f2cd4e4b39b5e9b857bc8c0a36c
Litva <litva2810@gmail.com>
2018-04-30 01:50:38 +0400

    Unit tests for Point class

---
+++ PointTest/PointTest/Point.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    class Point
+    {
+
+        /// Абсцисса точки "X"
+        public double x;
+
+        /// Свойство для чтения и записи абциссы точки
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        /// Ордината точки "Y"
+        public double y;
+
+        /// Свойство для чтения и записи ординаты точки
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        /// Значание скаляра 
+        private double r;
+
+        /// Свойство для чтения и записи значения скаляра
+        public double R
+        {
+            get { return r; }
+            set { r = value; }
+        }
+
+
+        /// Свойство, умножающее координаты точки на скаляр 
+        public double Scalar
+        {
+            set
+            {
+                x = x * value;
+                y = y * value;
+            }
+        }
+
+
+        /// Конструктор, создающий точку с заданными координатами и значение скаляра
+        public Point(double x, double y, double r)
+        {
+            this.x = x;
+            this.y = y;
+            this.r = r;
+        }
+
+        /// Конструктор, создающий точку с нулевыми координатами
+        public Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+
+        /// Метод выведения координаты точки на экран
+        public void Print()
+        {
+            Console.WriteLine("Координата точки = ({0};{1})", x, y);
+        }
+
+        /// Метод выведения на экран расстояния от начала координат до точки
+        public double Distance()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        /// Метод перемещения точки на вектор (а,b)
+        public void Move(double a, double b)
+        {
+            x += a;
+            y += b;
+        }
+
+        /// Индексатор,позволяющий по индексу 0 обращаться к полю x, по индексу 1 - к полю y.
+        public double this[int index]
+        {
+            get
+            {
+                if (index == 0) return x;
+                if (index == 1) return y;
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            set
+            {
+                if (index == 0) x = value;
+                if (index == 1) y = value;
+
+            }
+        }
+
+        /// Одновременно увеличивает значение полей x и y
+        public static Point operator ++(Point ob)
+        {
+            ob.x = ob.x + 1;
+            ob.y = ob.y + 1;
+            return ob;
+        }
+
+        /// Одновременно уменьшает значение полей x и y
+        public static Point operator --(Point ob)
+        {
+            try
+            {
+                ob.x = ob.x - 1;
+                ob.y = ob.y - 1;
+                if (ob.y < 0 | ob.x < 0) throw new System.ArithmeticException();
+            }
+            catch (System.ArithmeticException)
+            {
+                Console.WriteLine("Уменьшение не возможно !");
+                ob.x = ob.x + 1;
+                ob.y = ob.y + 1;
+            }
+            return ob;
+
+        }
+
+
+
+        /// Возращает значение true, если поле x = y, иначе false
+        public static bool operator ==(Point x, Point y)
+        {
+            return (x.X == y.X) && (x.Y == y.Y);
+        }
+
+
+        /// Возращает значение true, если поле x != y, иначе false
+        public static bool operator !=(Point x, Point y)
+        {
+            return (x.X != y.X) || (x.Y != y.Y);
+        }
+
+        /// Добавляет к значению поля x и y значение скаляра n
+        public static Point operator +(Point ob, double n)
+        {
+            ob.x = ob.x + n;
+            ob.y = ob.y + n;
+            return ob;
+        }
+
+    }
---
+++ PointTest/PointTest/UnitTestPoint.cs
@@ -0,0 +1,138 @@
+using System;
+using Xunit;
+
+namespace PointTest
+{
+    public class UnitTestPoint
+    {
+        private Point _pointForTest;
+
+        public UnitTestPoint()
+        {
+            _pointForTest = new Point(1, 2, 3);
+        }
+
+        [Fact]
+        public void InitializeClass()
+        {
+            double x = _pointForTest.X;
+            double y = _pointForTest.Y;
+            double r = _pointForTest.R;
+
+            Assert.Equal(x, 1.0);
+            Assert.Equal(y, 2.0);
+            Assert.Equal(r, 3.0);
+        }
+
+        [Fact]
+        public void GetByIndex()
+        {
+            double x = _pointForTest[0];
+            double y = _pointForTest[1];
+
+            Assert.Equal(x, 1.0);
+            Assert.Equal(y, 2.0);
+        }
+
+        [Fact]
+        public void SetX(){
+            _pointForTest.X = 5;
+            Assert.Equal(_pointForTest.X, 5);
+        }
+
+        [Fact]
+        public void SetY(){
+            _pointForTest.Y = 8;
+            Assert.Equal(_pointForTest.Y, 8);
+        }
+
+        [Fact]
+        public void SetRadius(){
+            _pointForTest.R = 50;
+            Assert.Equal(_pointForTest.R, 50);
+        }
+
+        [Fact]
+        public void PositionMove()
+        {
+            _pointForTest.Move(5, 5);
+
+            double x = _pointForTest.X;
+            double y = _pointForTest.Y;
+
+            Assert.Equal(x, 6);
+            Assert.Equal(y, 7);
+        }
+
+        [Fact]
+        public void MultipleOnScalar(){
+            _pointForTest.Scalar = 5;
+
+            double x = 1.0 * 5;
+            double y = 2.0 * 5;
+
+            Assert.Equal(x, _pointForTest.X);
+            Assert.Equal(y, _pointForTest.Y);
+        }
+
+        [Fact]
+        public void DistanceFromBeginning()
+        {
+            double x = _pointForTest.Distance();
+
+            Assert.Equal(x, Math.Sqrt(1.0 + 4.0));
+        }
+
+        [Fact]
+        public void PositionIncrement()
+        {
+            _pointForTest++;
+
+            double x = _pointForTest.X;
+            double y = _pointForTest.Y;
+
+            Assert.Equal(x, 2.0);
+            Assert.Equal(y, 3.0);
+        }
+
+        [Fact]
+        public void PositionDecrement()
+        {
+            _pointForTest--;
+
+            double x = _pointForTest.X;
+            double y = _pointForTest.Y;
+
+            Assert.Equal(x, 0.0);
+            Assert.Equal(y, 1.0);
+        }
+
+        [Fact]
+        public void PointsEquals(){
+            Point truePoint = new Point(1, 2, 3);
+            Point falsePoint = new Point(3, 2, 1);
+
+            Assert.True(truePoint == _pointForTest);
+            Assert.False(falsePoint == _pointForTest);
+        }
+
+        [Fact]
+        public void PointsUnequals()
+        {
+            Point truePoint = new Point(3, 2, 1);
+            Point falsePoint = new Point(1, 2, 3);
+
+            Assert.True(truePoint != _pointForTest);
+            Assert.False(falsePoint != _pointForTest);
+        }
+
+        [Fact]
+        public void SumPoints(){
+            Point result = new Point(3, 4, 3);
+            var sum = _pointForTest + 2;
+
+            Assert.Equal(sum.X, result.X);
+            Assert.Equal(sum.Y, result.Y);
+        }
+    }
+}
